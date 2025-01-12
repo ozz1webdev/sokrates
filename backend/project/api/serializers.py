c209ee2fe5465bca4098064ae7dfbc6100a8a1b9
ozz1webdev <ozz1webdev@gmail.com>
2025-01-12 19:29:21 +0100

    add serializers

---
+++ backend/project/api/serializers.py
@@ -0,0 +1,22 @@
+from django.rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+        instance.save()
+        return instance
+
+    def delete(self, instance):
+        instance.delete()
+        return instance
--- backend/project/project/settings.py
+++ backend/project/project/settings.py
@@ -20,7 +20,12 @@ SITE_ID = 1
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-CSRF_TRUSTED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000']
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
