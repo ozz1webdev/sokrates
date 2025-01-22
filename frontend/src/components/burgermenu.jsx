7492439b8f423f27e79aaad21cc059dd8e577e06
ozz1webdev <ozz1webdev@gmail.com>
2025-01-22 20:49:21 +0100

    add amimated.css lib

--- frontend/src/components/burgermenu.jsx
+++ frontend/src/components/burgermenu.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import styles from '../styles/burgermenu.module.css';
+import 'animate.css';
+import Logo from './logo.jsx';
 
 const BurgerMenu = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -11,14 +13,16 @@ const BurgerMenu = () => {
     return (
         <div className={styles.burgermenu}> 
             <div
-                className={`${styles.burger} ${isOpen ? styles.open : ''}`}
-                onClick={toggleMenu}
-            >
+                className={`${styles.burger} ${isOpen ? styles.open: ''}`}
+                onClick={toggleMenu}>
                 <div className={styles.bar}></div>
                 <div className={styles.bar}></div>
                 <div className={styles.bar}></div>
             </div>
             <nav className={`${styles.nav} ${isOpen ? styles.open : ''}`}>
+                <div className="{styles.logo} animate__animated animate__fadeIn">
+                    <Logo />
+                </div>
                 <ul>
                     <li><a href="#home">Home</a></li>
                     <li><a href="#posts">Posts</a></li>
@@ -27,6 +31,7 @@ const BurgerMenu = () => {
                     <li><a href="#registr">Register</a></li>
                 </ul>
             </nav>
+            {isOpen && <div className={styles.backdrop} onClick={toggleMenu}></div>}
         </div>
     );
 };
---
+++ frontend/src/components/logo.jsx
@@ -0,0 +1,15 @@
+const Logo = () => {
+
+    const style = {
+        backgroundColor: 'black',
+        width: '90px',
+        height: '90px',
+        color: 'white',
+    };
+
+    return (
+        <div style={style}>Logo</div>
+    );
+};
+
+export default Logo;
